b9f09f4cf86df61b685a1f0c1fe372ffe72eb29d
VOIDkuugeki <voidkuugeki@gmail.com>
2024-10-10 17:56:12 +0700

    fix search function

--- js/product.js
+++ js/product.js
@@ -56,51 +56,65 @@
             price:258000,
         },
     ];
-// đẩy mảng product vào local
+
+    // đẩy mảng product vào local
     function save(){
         localStorage.setItem('listProduct',JSON.stringify(product))
     }
-   //lấy sản phẩm 
-   function load(){
-       product = JSON.parse(localStorage.getItem('listProduct'));
-   } 
-   //xuất sản phẩm ra html
-   if (localStorage.getItem("listProduct") != null) {
-    load();
-}
+
+    //lấy sản phẩm 
+    function load(){
+        product = JSON.parse(localStorage.getItem('listProduct'));
+    } 
+
+    //xuất sản phẩm ra html
+    if (localStorage.getItem("listProduct") != null) {
+        load();
+    }
     save();
 
-   var listLocal = function(){
-       var listproduct ="";
-       for (var i in product){
-           var data = JSON.parse(JSON.stringify(product[i]))
-        var listproduct = '<div class="col-lg-3 col-md-6 col-sm-6 col-6 mt-3">';
-        listproduct += '<div class="card product p-2" styte="width:auto">'; 
-        listproduct += '<img class="card-img-top" src="img/' + data.img +'" alt="...">';
-        listproduct += '<div class="card-title product-title text-center h5" >'+data.name+'</div>';
-        listproduct += '<div class="price text-center h6">'+data.price+'₫</div>';
-        listproduct +=  '<span class="text-center add-to-cart btn btn-outline-warning add-cart" data-id="'+data.id+'" data-name="'+data.name+'" data-img="'+data.img+'" data-price="'+data.price+'" onclick="tks()">';
-        // listproduct +=  '<a class=" add-to-cart btn" data-name="'+product[i].name+'" data-price="'+product[i].price+'" onclick="tks()">';
-        listproduct +=  '<a>';
-        listproduct +=  '<i class="fas fa-cart-plus"></i>';
-        listproduct +=  '</a>';
-        listproduct +=  '</span>';
-        listproduct += '</div>';
-        listproduct += '</div>';
-
-        document.getElementById("banchay").innerHTML += listproduct;
-       }
-       Save();
-       
-   }
-
-   function searchList() {
-    var search = document.getElementById("search").value;
+    var listLocal = function() {
+        var listproduct = "";
+        for (var i in product) {
+            var data = JSON.parse(JSON.stringify(product[i]));
+            listproduct += generateProductHTML(data);
+        }
+        document.getElementById("banchay").innerHTML = listproduct;
+        save();
+    }
     
-    return product.filter(function (product) {
-        return product.name.toLowerCase().includes(search);
+    searchInput.addEventListener('input', function() {
+        const query = searchInput.value.toLowerCase();
+        const filteredProducts = product.filter(product => product.name.toLowerCase().includes(query));
+    
+        displayProducts(filteredProducts);
     });
-   }
+    
+    function displayProducts(products) {
+        const banchayElement = document.getElementById("banchay");
+        banchayElement.innerHTML = ''; // Empty the banchay element
+    
+        products.forEach(product => {
+            const data = JSON.parse(JSON.stringify(product));
+            banchayElement.innerHTML += generateProductHTML(data);
+        });
+    }
+    
+    function generateProductHTML(data) {
+        let listproduct = '<div class="col-lg-3 col-md-6 col-sm-6 col-6 mt-3">';
+        listproduct += '<div class="card product p-2" style="width:auto">';
+        listproduct += '<img class="card-img-top" src="img/' + data.img + '" alt="...">';
+        listproduct += '<div class="card-title product-title text-center h5">' + data.name + '</div>';
+        listproduct += '<div class="price text-center h6">' + data.price + '₫</div>';
+        listproduct += '<span class="text-center add-to-cart btn btn-outline-warning add-cart" data-id="' + data.id + '" data-name="' + data.name + '" data-img="' + data.img + '" data-price="' + data.price + '" onclick="tks()">';
+        listproduct += '<a>';
+        listproduct += '<i class="fas fa-cart-plus"></i>';
+        listproduct += '</a>';
+        listproduct += '</span>';
+        listproduct += '</div>';
+        listproduct += '</div>';
+        return listproduct;
+    }
 
 listLocal();
 
